5d8ec40fa69c1cf50d9d6aae3f4fb3df667e9287
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2021-07-13 13:55:33 +0530

    created hero image component with styles

---
+++ src/components/HeroImage/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+/*Styles*/
+import { Wrapper, Content, Text } from "../HeroImage/HeroImage.styles";
+
+const HeroImage = ({ image, title, text }) => {
+  // console.log(image);
+  // console.log(title);
+  // console.log(text);
+  return (
+    <Wrapper image={image}>
+      <Content>
+        <Text>
+          <h1>{title}</h1>
+          <p>{text}</p>
+        </Text>
+      </Content>
+    </Wrapper>
+  );
+};
+
+export default HeroImage;
