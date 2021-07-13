d8c9d9ba407550fa64da11bd729b7d94968ce9f1
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2021-07-13 18:06:46 +0530

    hooked search to api

--- src/hooks/useHomeFetch.js
+++ src/hooks/useHomeFetch.js
@@ -37,10 +37,11 @@ export const useHomeFetch = () => {
     setLoading(false);
   };
 
-  /*Initial Render */
+  /*Initial Render & Search*/
   useEffect(() => {
-    fetchMovies(1);
-  }, []);
+    setState(initialState);
+    fetchMovies(1, searchTerm);
+  }, [searchTerm]);
 
-  return { state, loading, error, setSearchTerm };
+  return { state, loading, error, setSearchTerm, searchTerm };
 };
