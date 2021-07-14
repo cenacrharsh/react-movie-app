b42f4b4d8a196de8364954cb6c0d72b9fee19aba
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2021-07-14 13:13:14 +0530

    added Actor grid

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -10,6 +10,7 @@ import Spinner from "./Spinner";
 import BreadCrumb from "./BreadCrumb.js";
 import MovieInfo from "./MovieInfo.js";
 import MovieInfoBar from "./MovieInfoBar";
+import Actor from "./Actor";
 
 /*Hook*/
 import { useMovieFetch } from "../hooks/useMovieFetch";
@@ -38,6 +39,20 @@ const Movie = () => {
         budget={movie.budget}
         revenue={movie.revenue}
       />
+      <Grid header="Actors">
+        {movie.actors.map((actor) => (
+          <Actor
+            key={actor.credit_id}
+            name={actor.name}
+            character={actor.character}
+            imageUrl={
+              actor.profile_path
+                ? `${IMAGE_BASE_URL}${POSTER_SIZE}${actor.profile_path}`
+                : NoImage
+            }
+          />
+        ))}
+      </Grid>
     </>
   );
 };
