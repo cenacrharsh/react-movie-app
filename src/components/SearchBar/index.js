ad8b39ecc3194a084e65f6426148ab5a651d4c23
Kumar Harsh <harsh.nishant.superstar@gmail.com>
2021-07-14 17:16:25 +0530

    refactored SearchBar using class component

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,5 +1,57 @@
-import React, { useState, useEffect, useRef } from "react";
-import PropTypes from "prop-types";
+// import React, { useState, useEffect, useRef } from "react";
+// import PropTypes from "prop-types";
+
+// /*Image*/
+// import searchIcon from "../../images/search-icon.svg";
+
+// /*Styles*/
+// import { Wrapper, Content } from "./SearchBar.styles";
+
+// const SearchBar = ({ setSearchTerm }) => {
+//   const [state, setState] = useState("");
+//   const initial = useRef(true);
+
+//   /* useRef doesn't trigger a rerender upon being changed and we also don't need a setter for changing it*/
+
+//   /*We want to trigger useEffect only when user types something and not on first mount*/
+
+//   useEffect(() => {
+//     if (initial.current) {
+//       initial.current = false;
+//       return;
+//     }
+//     const timer = setTimeout(() => {
+//       setSearchTerm(state);
+//     }, 500);
+//     return () => clearTimeout(timer);
+//   }, [setSearchTerm, state]);
+
+//   return (
+//     <Wrapper>
+//       <Content>
+//         <img src={searchIcon} alt="search-icon" />
+//         <input
+//           type="text"
+//           placeholder="Search Movie"
+//           onChange={(event) => {
+//             setState(event.currentTarget.value);
+//           }}
+//           value={state}
+//         />
+//       </Content>
+//     </Wrapper>
+//   );
+// };
+
+// SearchBar.propTypes = {
+//   callback: PropTypes.func,
+// };
+
+// export default SearchBar;
+
+// Refactoring Code a/q to Class Components
+
+import React from "react";
 
 /*Image*/
 import searchIcon from "../../images/search-icon.svg";
@@ -7,44 +59,42 @@ import searchIcon from "../../images/search-icon.svg";
 /*Styles*/
 import { Wrapper, Content } from "./SearchBar.styles";
 
-const SearchBar = ({ setSearchTerm }) => {
-  const [state, setState] = useState("");
-  const initial = useRef(true);
+class SearchBar extends React.Component {
+  state = { value: "" };
+  timeout = null;
 
-  /* useRef doesn't trigger a rerender upon being changed and we also don't need a setter for changing it*/
+  componentDidUpdate(_prevProps, prevState) {
+    if (this.state.value !== prevState.value) {
+      const { setSearchTerm } = this.props;
 
-  /*We want to trigger useEffect only when user types something and not on first mount*/
+      clearTimeout(this.timeout);
 
-  useEffect(() => {
-    if (initial.current) {
-      initial.current = false;
-      return;
+      this.timeout = setTimeout(() => {
+        const { value } = this.state;
+        setSearchTerm(value);
+      }, 500);
     }
-    const timer = setTimeout(() => {
-      setSearchTerm(state);
-    }, 500);
-    return () => clearTimeout(timer);
-  }, [setSearchTerm, state]);
+  }
 
-  return (
-    <Wrapper>
-      <Content>
-        <img src={searchIcon} alt="search-icon" />
-        <input
-          type="text"
-          placeholder="Search Movie"
-          onChange={(event) => {
-            setState(event.currentTarget.value);
-          }}
-          value={state}
-        />
-      </Content>
-    </Wrapper>
-  );
-};
+  render() {
+    const { value } = this.state;
 
-SearchBar.propTypes = {
-  callback: PropTypes.func,
-};
+    return (
+      <Wrapper>
+        <Content>
+          <img src={searchIcon} alt="search-icon" />
+          <input
+            type="text"
+            placeholder="Search Movie"
+            onChange={(event) => {
+              this.setState({ value: event.currentTarget.value });
+            }}
+            value={value}
+          />
+        </Content>
+      </Wrapper>
+    );
+  }
+}
 
 export default SearchBar;
